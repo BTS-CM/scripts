13421503717c0866f2f4828baefb30ecae234f59
grcgrc <grctestemail@gmail.com>
2017-09-30 13:37:36 +0100

    Added excel spreadsheet hertz calculator
    Should help figure out a sensible amplitude

--- hertz-feed.py
+++ hertz-feed.py
@@ -19,19 +19,20 @@ hertz_usd = reference_asset_value + (amplitude * math.sin(((((current_time - ref
 market = Market("USD:BTS") # Set reference market to USD:BTS
 price = market.ticker()["quoteSettlement_price"] # Get Settlement price of USD
 price.invert() # Switching from USD/BTS to BTs/USD?
-hertz = Price(hertz_usd, "USD/HERTZ") # Limit the hertz_usd decimal places?
+hertz = Price(hertz_usd, "USD/HERTZ") # Limit the hertz_usd decimal places & convert from float.
 
 hertz_bts = hertz / price # Calculate HERTZ price in BTS
 
-# Some outputs
+# Some printed outputs 
 print("Price of HERTZ in USD: {}".format(hertz))
 print("Price of USD in BTS: {}".format(price))
 print("Price of HERTZ in BTS: {}".format(hertz_bts))
 
-# Unlock the wallet
+# Unlock the Bitshares wallet
 hertz.bitshares.wallet.unlock(getpass())
 
-# Publish the price feed
+# Publish the price feed to the BTS DEX
+# Make sure you change the 'account' before executing.
 pprint(hertz.bitshares.publish_price_feed(
     "HERTZ",
     hertz_bts,
