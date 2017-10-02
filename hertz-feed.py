6c16476a5ed3e6190f99a73a76bd9887e833ceda
grcgrc <grctestemail@gmail.com>
2017-10-02 23:37:35 +0100

    Added alternative API nodes
    If an API node goes down, price feed publishers can easily specify an alternative.

--- hertz-feed.py
+++ hertz-feed.py
@@ -1,17 +1,30 @@
 from getpass import getpass
 from pprint import pprint
 from bitshares.asset import Asset
+from bitshares import BitShares
 from bitshares.block import Block
+from bitshares.instance import set_shared_bitshares_instance
 from bitshares.price import Price
 from bitshares.market import Market
 import pendulum
 import math
 
-amplitude = 0.5 # 50% fluctuating the price feed $+-0.50
-current_time = pendulum.now().timestamp() # Current timestamp for reference within the hertz script
+bitshares_api_node = BitShares(
+    # If the connected API node is down, uncomment one of the
+    #"wss://node.bitshares.eu/ws"
+    #"wss://dexnode.net/ws"
+    #"wss://bitshares.openledger.info/ws"
+    "wss://bitshares.crypto.fans/ws"
+    #"wss://openledger.hk/ws"
+    )
+
+set_shared_bitshares_instance(bitshares_api_node)
+
 reference_timestamp = pendulum.parse(Block(1)['timestamp']).timestamp() # Retrieving the Bitshares2.0 genesis block timestamp
 period = pendulum.SECONDS_PER_DAY * 30.43 # 30.43 days converted to an UNIX timestamp
 reference_asset_value = 1.00 # $1.00 USD
+amplitude = 0.5 # 50% fluctuating the price feed $+-0.50
+current_time = pendulum.now().timestamp() # Current timestamp for reference within the hertz script
 waveform = math.sin(((((current_time - reference_timestamp)/period) % 1) * period) * ((2*math.pi)/period)) # Only change for an alternative HERTZ ABA.
 
 # Get HERTZ price in USD
